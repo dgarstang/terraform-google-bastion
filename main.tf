334c7f40a49be025f48902e373f7f208555c813c
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-14 18:58:47 +1000

    ...

--- main.tf
+++ main.tf
@@ -28,6 +28,32 @@ resource "google_compute_instance" "bastion" {
   metadata_startup_script = <<-EOT
     #!/bin/bash
     apt-get update
+    apt-get install -y wireguard
+
+    umask 077
+    wg genkey | tee /etc/wireguard/privatekey | wg pubkey > /etc/wireguard/publickey
+
+    PRIVATE_KEY=$(cat /etc/wireguard/privatekey)
+
+    systemctl stop wg-quick@wg0
+
+    cat <<EOF > /etc/wireguard/wg0.conf
+    [Interface]
+    Address = 10.0.0.1/24
+    ListenPort = 51820
+    PrivateKey = $PRIVATE_KEY
+    SaveConfig = false
+
+    [Peer]
+    PublicKey = rkysLZbnTa6mJOzeabZuF8fL8uiKWpkXUXx/tgdSEWM=
+    AllowedIPs = 10.0.0.2/32
+    EOF
+
+    sysctl -w net.ipv4.ip_forward=1
+    echo "net.ipv4.ip_forward=1" >> /etc/sysctl.conf
+
+    systemctl enable wg-quick@wg0
+    systemctl start wg-quick@wg0
   EOT
 
 }
