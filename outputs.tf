2a415f2756eaad7398022cf77f44f9c591fadfee
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-15 15:49:36 +1000

    ...

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "bastion_public_ip" {
+  description = "Public IP address of the bastion instance"
   value = google_compute_address.bastion_public_ip.address
+}
+
+output "bastion_private_ip" {
+  description = "Private IP address of the bastion instance"
+  value       = google_compute_instance.bastion.network_interface[0].network_ip
 }
