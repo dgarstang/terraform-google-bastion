370bde15ece8c8131c371b6cd9627b16cfa8f24d
Douglas Garstang <dgarstang@192-168-1-113.tpgi.com.au>
2025-04-17 08:46:49 +1000

    ...

--- firewall.tf
+++ firewall.tf
@@ -11,20 +11,6 @@ resource "google_compute_firewall" "allow_ssh_from_iap" {
 }
 */
 
-resource "google_compute_firewall" "delete_default_ingress" {
-  name    = "ingress-from-home"
-  network = var.network_name
-  lifecycle {
-    create_before_destroy = true
-  }
-  allow { # Add an empty allow block.  This is required, but won't allow any traffic
-    protocol = "tcp"
-    ports    = ["22"]
-  }
-  source_ranges = ["202.92.122.131/32"]
-  target_tags   = ["allow-ssh"]
-}
-
 resource "google_compute_firewall" "wireguard" {
   name    = "allow-wireguard"
   network = "custom"
---
+++ firewall2.tf
@@ -0,0 +1,65 @@
+resource "google_compute_firewall" "control_plane_to_nodes" {
+  name          = "allow-control-plane-to-nodes"
+  network       = var.network_name
+  direction     = "INGRESS"
+  source_ranges = ["35.191.0.0/16", "130.211.0.0/22"]
+  priority      = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["443", "10250", "8443"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["8472"]
+  }
+  allow {
+    protocol = "icmp"
+  }
+  description = "Allow GKE control plane to communicate with nodes"
+}
+
+resource "google_compute_firewall" "nodes_to_control_plane" {
+  name               = "allow-nodes-to-control-plane"
+  network            = var.network_name
+  direction          = "EGRESS"
+  destination_ranges = ["172.16.0.0/12"] # Replace with actual control plane private range
+  priority           = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
+  }
+  description = "Allow nodes to reach the GKE control plane over private IP"
+}
+
+resource "google_compute_firewall" "node_to_node" {
+  name          = "allow-node-to-node"
+  network       = var.network_name
+  direction     = "INGRESS"
+  priority      = 1000
+  allow {
+    protocol = "all"
+  }
+  description = "Allow all traffic between GKE nodes"
+}
+
+resource "google_compute_firewall" "node_egress_internet" {
+  name               = "allow-node-egress-internet"
+  network            = var.network_name
+  direction          = "EGRESS"
+  destination_ranges = ["0.0.0.0/0"]
+  priority           = 1000
+  allow {
+    protocol = "tcp"
+    ports    = ["80", "443"]
+  }
+  allow {
+    protocol = "udp"
+    ports    = ["53"]
+  }
+  allow {
+    protocol = "tcp"
+    ports    = ["53"]
+  }
+  description = "Allow GKE nodes to access internet"
+}
+
